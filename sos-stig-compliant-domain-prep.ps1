59d36305716479fe282373f3a9f29e803a68f14d
fT5SgrjXeV8khTNVgQmD <4913771+simeononsecurity@users.noreply.github.com>
2020-09-08 20:24:01 -0500

    Simplified Primary Script and Removed Legacy LGPO Text Script
    Now capable of importing any GPO listed under .\Files\GPOs

--- sos-stig-compliant-domain-prep.ps1
+++ sos-stig-compliant-domain-prep.ps1
@@ -7,23 +7,15 @@ $ErrorActionPreference= 'silentlycontinue'
 Write-Output "Elevating priviledges for this process"
 do {} until (Elevate-Privileges SeTakeOwnershipPrivilege)
 
-Foreach ($dodgpo in Get-ChildItem .\Files\GPOs\DoD) {
-    $gpopath = "$(Get-Location)\Files\GPOs\DoD\$dodgpo"
-    Write-Output "Importing $dodgpo"
-    New-GPO -Name "$dodgpo" -Comment "Created by simeononsecurity.ch" 
-    Import-GPO -BackupGpoName "$dodgpo" -Path "$gpopath" -TargetName "$dodgpo" -CreateIfNeeded
-    }
+foreach ($gpocategory in Get-ChildItem "$(Get-Location)\Files\GPOs") {
+    
+    Write-Output "Importing $gpocategory GPOs"
 
-Foreach ($nsagpo in Get-ChildItem .\Files\GPOs\NSACyber) {
-    $gpopath = "$(Get-Location)\Files\GPOs\NSACyber\$nsagpo"
-    Write-Output "Importing $nsagpo"
-    New-GPO -Name "$nsagpo" -Comment "Created by simeononsecurity.ch" 
-    Import-GPO -BackupGpoName "$nsagpo" -Path "$gpopath" -TargetName "$nsagpo" -CreateIfNeeded
+    Foreach ($gpo in (Get-ChildItem "$(Get-Location)\Files\GPOs\$gpocategory")) {
+        $gpopath = "$gposdir\$gpocategory\$gpo"
+        Write-Output "Importing $gpo"
+        New-GPO -Name "$gpo" -Comment "Created by simeononsecurity.ch" 
+        Import-GPO -BackupGpoName "$gpo" -Path "$gpopath" -TargetName "$gpo" -CreateIfNeeded 
     }
+}
 
-Foreach ($sosgpo in Get-ChildItem .\Files\GPOs\SoS) {
-    $gpopath = "$(Get-Location)\Files\GPOs\Files\GPOs\SoS\$sosgpo"
-    Write-Output "Importing $sosgpo"
-    New-GPO -Name "$sosgpo" -Comment "Created by simeononsecurity.ch" 
-    Import-GPO -BackupGpoName "$sosgpo" -Path "$gpopath" -TargetName "$sosgpo" -CreateIfNeeded 
-    }
