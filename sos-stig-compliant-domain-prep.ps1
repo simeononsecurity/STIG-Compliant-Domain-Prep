6d08e4b072eed9fae3e5d37af09a547bed428890
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2023-01-29 07:24:28 +0000

    Update sos-stig-compliant-domain-prep.ps1

--- sos-stig-compliant-domain-prep.ps1
+++ sos-stig-compliant-domain-prep.ps1
@@ -11,13 +11,28 @@ do {} until (Elevate-Privileges SeTakeOwnershipPrivilege)
 if ((Get-Location).Path -NE $PSScriptRoot) { Set-Location $PSScriptRoot }
 
 #Import PolicyDefinitions
-    #Import Locally
-Start-Job -ScriptBlock {takeown /f C:\WINDOWS\PolicyDefinitions /r /a; icacls C:\WINDOWS\PolicyDefinitions /grant Administrators:(OI)(CI)F /t; Copy-Item -Path .\Files\PolicyDefinitions\* -Destination C:\Windows\PolicyDefinitions -Force -Recurse -ErrorAction SilentlyContinue}
-    #Import to Central Store
-Foreach ($sysvolpath in Get-ChildItem "C:\Windows\SYSVOL\sysvol") {
-    New-Item "C:\Windows\SYSVOL\sysvol\$sysvolpath\Policies\PolicyDefinitions\" -Force
-    Copy-Item -Path "$(Get-Location)\Files\PolicyDefinitions\*" -Destination "C:\Windows\SYSVOL\sysvol\$sysvolpath\Policies\PolicyDefinitions\" -Force -Recurse
+# Import to Central Store
+try {
+    Start-Job -ScriptBlock {
+        takeown /f C:\WINDOWS\PolicyDefinitions /r /a -ErrorAction Stop
+        icacls C:\WINDOWS\PolicyDefinitions /grant Administrators:(OI)(CI)F /t -ErrorAction Stop
+        Copy-Item -Path .\Files\PolicyDefinitions\* -Destination C:\Windows\PolicyDefinitions -Force -Recurse -ErrorAction Stop
+    } -ErrorAction Stop -AsJob -UseTransaction -Verbose
+    # Get all child items of the "C:\Windows\SYSVOL\sysvol" directory
+    $sysvolpaths = Get-ChildItem "C:\Windows\SYSVOL\sysvol" -ErrorAction Stop
+    # Iterate through each child item
+    foreach ($sysvolpath in $sysvolpaths) {
+        # Create the "PolicyDefinitions" directory if it doesn't exist
+        if (!(Test-Path "C:\Windows\SYSVOL\sysvol\$sysvolpath\Policies\PolicyDefinitions\")) {
+            New-Item "C:\Windows\SYSVOL\sysvol\$sysvolpath\Policies\PolicyDefinitions\" -Force
+        }
+        # Copy the files to the "PolicyDefinitions" directory
+        Copy-Item -Path "$(Get-Location)\Files\PolicyDefinitions\*" -Destination "C:\Windows\SYSVOL\sysvol\$sysvolpath\Policies\PolicyDefinitions\" -Force -Recurse
+    }
+} catch {
+    Write-Error "An error occurred: $($_.Exception.Message)"
 }
+
 
 #Import GPOS into GPMC
 $gposdir = "$(Get-Location)\Files\GPOs"
